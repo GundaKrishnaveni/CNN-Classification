8327cdbba489b175474da59f5a5b3d5ad5a0f9c2
GundaKrishnaveni <154601613+GundaKrishnaveni@users.noreply.github.com>
2025-12-31 14:31:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+import numpy as np
+from tensorflow.models import sequential
+from tensorflow.keras.layers import conv2D,Activation,MaxPooling2D,Dense,Flatten
+from tensorflow.keras.preprocessing.img import ImageDataGenerator
